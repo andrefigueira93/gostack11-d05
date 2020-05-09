bde0bb037406e4d44cf17673ede9257fe46a1ee2
andrefigueira93 <andrefigueira93@gmail.com>
2020-05-09 12:56:54 -0300

    Entrega do Projeto

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,33 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('Invalid Operation');
+    }
+    if (!value || typeof value !== 'number') {
+      throw new Error('Invalid Value');
+    }
+    if (!title) {
+      throw new Error('You must inform a title');
+    }
+    if (typeof title !== 'string') {
+      throw new Error('Title must be a text');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new Error("You don't have enough balance for this operation");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
